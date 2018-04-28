72670de5d05964f31526c73c0e9d4443aa3725d1
Tom Golden <tbjgolden@gmail.com>
2018-04-28 16:40:43 -0700

    yay

--- src/pages/Blog/posts/index.js
+++ src/pages/Blog/posts/index.js
@@ -2,6 +2,7 @@ import nicemark from '../../../lib/nicemark.min';
 
 import _2018_01_13 from './2018_01_13.md';
 import _2018_04_26 from './2018_04_26.md';
+import _2018_04_27 from './2018_04_27.md';
 
 export const posts = [
   {
@@ -14,6 +15,12 @@ export const posts = [
     title: 'It\'s time to get back with your ex (units)',
     date: '2018/04/26',
     html: nicemark(_2018_04_26.replace(/^\s*#{1,6} ?([^\n]+)/, '')),
+    url: 'its-time-to-get-back-with-your-ex-units'
+  },
+  {
+    title: 'It\'s time to get back with your ex (units)',
+    date: '2018/04/27',
+    html: nicemark(_2018_04_27.replace(/^\s*#{1,6} ?([^\n]+)/, '')),
     url: 'its-time-to-get-back-with-your-ex-units'
   }
 ].sort((a, b) => a.date < b.date);
